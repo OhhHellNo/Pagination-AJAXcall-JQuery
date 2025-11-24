9b08ded54ab28fa61254ff350bec4b1f2caeab1c
viperofficailll <sharadhapokharel777@gmal.com>
2025-11-24 07:45:11 +0545

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pagination.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pagination.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
